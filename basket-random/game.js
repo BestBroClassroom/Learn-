1900bb7d66c0323a4b2a20ed14924376ef8a3af2
BestBroClassroom <kristian.humphries@millburnacademy.org.uk>
2025-04-06 18:18:26 +0100

    Update game.js

--- basket-random/game.js
+++ basket-random/game.js
@@ -2,14 +2,14 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Basket Random</title>
+  <title>Basket Random</title> <!-- Title of the page -->
   <style>
     body { margin: 0; overflow: hidden; }
-    canvas { display: block; background: #000; }
+    canvas { display: block; background: #000; } <!-- Black canvas -->
   </style>
 </head>
 <body>
-  <canvas id="gameCanvas" width="800" height="600"></canvas>
-  <script src="https://gist.githubusercontent.com/BestBroClassroom/05e79105cbc125381d619880fabd60d6/raw/775b00e337d82a1f8aba25054c5ee3b74242b0d4/game.js"></script>
+  <canvas id="gameCanvas" width="800" height="600"></canvas> <!-- Where the game appears -->
+  <script src="https://gist.githubusercontent.com/BestBroClassroom/05e79105cbc125381d619880fabd60d6/raw/775b00e337d82a1f8aba25054c5ee3b74242b0d4/game.js"></script> <!-- Your JS file -->
 </body>
 </html>
